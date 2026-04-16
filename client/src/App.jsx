d8ff5987831ef8230f0dd219c0103bd8862efd61
Mousi <2023-1-60-122@std.ewubd.edu>
2026-04-17 03:30:03 +0600

    update backend url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
  });
 
  useEffect(() => {
-   fetch("http://localhost:5000/projects")
+   fetch("https://portfolio-workshop-ck8z.onrender.com/projects")
      .then((res) => res.json())
      .then((data) => setProjects(data))
      .catch((err) => console.error(err));
@@ -26,7 +26,7 @@ function App() {
  const handleSubmit = async (e) => {
    e.preventDefault();
 
-   const response = await fetch("http://localhost:5000/contact", {
+   const response = await fetch("https://portfolio-workshop-ck8z.onrender.com/contact", {
      method: "POST",
      headers: {
        "Content-Type": "application/json"
